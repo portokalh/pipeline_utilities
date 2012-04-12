2e98ab2e54dbff91203deddce2889d36af64cf44
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2012-04-12 17:04:26 +0000

    pipeline_utilities bug fix again with startup.m
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/pipeline_utilities/trunk@898 d41079a3-eaa1-4f09-8506-b324c47726bb

--- startup.m
+++ startup.m
@@ -1,3 +1,3 @@
 addpath /pipe_home/script/matlab_functions_local
-addpath /pipe_home/matlab_library/NIFTI_20110921
+addpath /pipe_home/matlab_functions_external/NIFTI_20110921
 addpath /pipe_home/script/matlab_functions_local/T2WsuseptibilityReg/
