cc392f0188cc08c813553692945e33ed226ee11b
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2012-04-13 16:59:53 +0000

    seg_pipe_mc fix some errors
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/pipeline_utilities/trunk@901 d41079a3-eaa1-4f09-8506-b324c47726bb

--- convert_to_nifti_util.pm
+++ convert_to_nifti_util.pm
@@ -68,7 +68,7 @@ sub convert_to_nifti_util {
   }
  
   if ($xdim eq "NO_KEY" || $zdim eq "NO_KEY" || $ydim eq "NO_KEY" || $xfov_mm eq "NO_KEY"|| $yfov_mm eq "NO_KEY"|| $zfov_mm eq "NO_KEY") {
-      error_out("Could not find good value for xyz or xyz fov\n\tx=$xdim, y=$ydim, z=$zdim, xfov=$xfov_mm, yfov=$yfov_mm, zfov=$zfov_mm\n");
+      error_out("Could not find good value for xyz or xyz fov\n\tx=$xdim, y=$ydim, z=$zdim, xfov=$xfov_mm, yfov=$yfov_mm, zfov=$zfov_mm\n") unless($ggo==0);
   }
 
 
