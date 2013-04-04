c381b3ac78bef05d60065c43061f0b314d3764f0
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2013-04-04 19:10:35 +0000

    pipeline_utilities upated startup.m and added mri_calc perl to matlab stub
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/pipeline_utilities/trunk@1224 d41079a3-eaa1-4f09-8506-b324c47726bb

--- startup.m
+++ startup.m
@@ -2,5 +2,8 @@ addpath /pipe_home/script/matlab_functions_local
 addpath /pipe_home/matlab_functions_external/NIFTI_20110921
 addpath /pipe_home/script/matlab_functions_local/T2WsuseptibilityReg/
 addpath /recon_home/script/dir_radish/modules/matlab
+addpath(genpath('/recon_home/script/dir_radish/modules/matlab/mathworks'))
 addpath /recon_home/script/dir_radish/modules/matlab/agilent
 addpath /recon_home/script/dir_radish/modules/matlab/radish_filter
+addpath /recon_home/script/dir_radish/modules/matlab/mr_relaxation_calc/
+addpath /recon_home/script/dir_radish/modules/matlab/civm_matlab_common_utils/
