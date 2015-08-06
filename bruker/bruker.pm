65d04a8c5230f57276f82dc482315b0f1756e5f0
James Cook <jamesjcook@users.noreply.github.com>
2015-08-06 11:49:04 -0400

    merge fix

--- bruker/bruker.pm
+++ bruker/bruker.pm
@@ -104,7 +104,7 @@ my $VERSION = "2013/04/29";
 my $COMMENT = "Bruker meta data functions";
 use vars qw(@knownmethods);
 
-my @cartesian_3D_methods= qw( MGE RARE MSME DtiStandard dtiStandard_1 GEFC  ); 
+my @cartesian_3D_methods= qw( FLASH MGE RARE MSME DtiStandard dtiStandard_1 GEFC ); 
 my @cartesian_2D_methods= qw(MDEFT ) ;
 my @radial_methods=qw( UTE UTE3D ute3d_keyhole Bruker:SPIRAL Bruker:DtiSpiral);
 push(@knownmethods,@cartesian_3D_methods);
