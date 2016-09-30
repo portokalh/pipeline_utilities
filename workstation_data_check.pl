bd9146dea0dea0e7cb607678ec30946d2ba270e7
James Cook <jamesjcook@users.noreply.github.com>
2016-09-30 16:37:58 -0400

    Does a better job saving the md5's now by using the file extension in the name of the md5 file too. Now looks at files which fail and lets user know about it better, and cohesively at the	  end. A start	  to a cleanup routine exists, which would rename the old file to oldfile_mod_date.md5, but its not done, as once I cleaned up the filenames, i had no more out of sync files.

--- workstation_data_check.pl
+++ workstation_data_check.pl
@@ -5,6 +5,7 @@ use Env qw(PIPELINE_SCRIPT_DIR);
 # generic incldues
 use Cwd qw(abs_path);
 use File::Basename;
+use Data::Dump qw(dump);
 use lib dirname(abs_path($0));
 use Env qw(RADISH_PERL_LIB);
 use vars qw($PIPELINE_VERSION $PIPELINE_NAME $PIPELINE_DESC $HfResult $GOODEXIT $BADEXIT ); #$test_mode
@@ -22,17 +23,25 @@ if (! defined($RADISH_PERL_LIB)) {
 use lib split(':',$RADISH_PERL_LIB);
 
 require pipeline_utilities ;
-require retrieve_archived_data;
+#require retrieve_archived_data;
 require Headfile;
-#require image_math;
-#require registration;
-
-#require apply_coil_bias_to_all;
-#require apply_noise_reduction_to_all;
 
 
-my $file_pat="(.*\.nii(?:\.gz)?)|(.*\.txt)|(.*\.xml)\$";
 
+use civm_simple_util qw(sleep_with_countdown  $debug_val mod_time);
+$debug_val=25;
+
+
+my %opt;
+use Getopt::Std;
+if (! getopts('ud:', \%opt)) {
+    usage_message("Problem with command line options.\n");
+}
+
+
+
+#my $file_pat='((.*\.nii(?:\.gz)?)|(.*\.txt)|(.*\.xml))$';
+my $file_pat="((.*\.nii(?:\.gz)?)|(.*\.txt)|(.*\.xml))\$";
 #  my ($identifier,@required_values) = @_;#runno
 #my ($local_input_dir, $local_work_dir, $local_result_dir, $result_headfile, $EC)=new_get_engine_dependencies('PU_TEST',());
 my $EC      =load_engine_deps();
@@ -57,12 +66,15 @@ my @dirs_to_check=();
 for(my $fn=0;$fn<=$#atlas_dir_contents;$fn++){
     my $thing=$EC->get_value("engine_data_directory")."/atlas".'/'.$atlas_dir_contents[$fn];
     if ( -d $thing && !  -l $thing ) {
-	if (-r $thing ){
+	if (-r $thing ){ 
+	    # Directory, and readable
 	    push(@dirs_to_check,$thing);
 	} else {
+	    # Directory, but unreadable
 	    print("Ignoring $thing: could not read.\n");
 	}
     } else {
+	# File, but its a link, check same as files.
 	if ( -l $thing) {
 	    print("Adding link $thing\n");
 	    push (@list,$thing);
@@ -88,14 +100,88 @@ if ( $#list < 0  ) {
     print(join(" ",@list)."\n");
 }
     
+my @fail_lists=();
 
+{ 
+
+=pod 
+
+hoaoa.pm
+aoaref_get_single
+aoaref_get_subarray
+aoaref_get_length
+aoaref_get_sub_length
+aoaref_to_singleline
+aoaref_to_printline
+array_find_by_length
+aoa_hash_to_headfile
+display_header
+display_header_entry
+single_find_by_value
+printline_to_aoa
+
+=cut 
+
+}
+
+# lets orgaznize failures by directory. 
+# for each directory, lets make an array as we go. 
+# After we've looked at all fo them, we'll dump that file to a failure file in the the directory.
+# Then we'll dump the commands to rename current md5's 
+my $fail_dirs; # this'll be a hash_ref to hash of arrays, I guess it could just be a hash proper...
+print("checking ".scalar(@list)." files\n"); 
+#exit;
+#for my $file (@list[0..60] ) {
 for my $file (@list ) {
     #print("Test, checksum of file $file\n");
-    my $exit_code=data_integrity($file);
-    if ( ! $exit_code ) {
+    #my $exit_code=data_integrity($file);
+    my $integrity=data_integrity($file,2);
+    # if( ! $integrity ) { 	print(" failure ".$file."\n"); }
+    if ( length($integrity)>1 || ! $integrity ) {
 	print(" failure ".$file."\n");
-	
+	my ($p,$n,$e)=fileparts($file,2);
+	#push(@{$fail_dirs->{$p}},$n.$e);
+	$fail_dirs->{$p}->{$n.$e}=$integrity;
     } else { 
 	print("$file good!\n"); 
+    }
+}
+
+#dump($fail_dirs);
+print("Waiting to proceed with auto fix of move md5 to mod date, and save new\n");
+#$debug_val=10;
+if ( defined $opt{u} ) { # -u update md5's
+    sleep_with_countdown(8);
+}
+for my $dir (keys %$fail_dirs){
+    #my $files=$fail_dirs->{$dir};
+    my @files=keys( %{$fail_dirs->{$dir}});
+    #dump(@files);
+    #next;
+    ##### INSERT FAILURE HANDLING HERE!
+    print("$dir had ".scalar(@files)." failures\n");
+    if ( 1 || defined $opt{u} ) { # -u update md5's
+	for my $ne (@files) {
+	    my ($p,$n,$e)=fileparts($dir.$ne,3);
+	    #my $chk_file=$p.$n.'.md5'; #previous checksum format where we dropped the file extension.
+	    my $chk_file=$p.$n.$e.".md5";
+	    #Headfile.pm:    #my ($s,$m,$h,$mday,$mon,$year,$wd,$yd,$is) = localtime(time);
+	    #my ($s,$m,$h,$mday,$mon,$year,$w,$y,$isdst) = localtime(mod_time($chk_file));
+	    print("checking file $chk_file ");
+	    my ($sec, $min, $hour, $day,$month,$year) = (localtime(mod_time($chk_file)))[0,1,2,3,4,5]; 
+	    $year+=1900;
+	    #use DateTime;
+	    #my $dt = DateTime->from_epoch( epoch => mod_time($chk_file));
+	    #my $year= $dt->year;
+	    #my $month=$dt->month;
+	    #my $day=$dt->day;
+	    #my $hour=$dt->hour;
+	    #my $min=$dt->min;
+	    #my $sec=$dt->sec;
+
+	    print("-> $p$n.$year-$month-$day.md5\n");# $year-$month-$day $hour:$min:$sec \n");
+	    
+	}
+
     }
 }
